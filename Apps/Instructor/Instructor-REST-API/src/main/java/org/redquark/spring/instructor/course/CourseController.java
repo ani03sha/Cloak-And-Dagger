0fb5030327b2b26f65c909b21bcab45baf25d0b2
Anirudh Sharma <anirudh03sharma@gmail.com>
2020-02-22 20:08:19 +0530

    Added the code for updating and deleting

--- Apps/Instructor/Instructor-REST-API/src/main/java/org/redquark/spring/instructor/course/CourseController.java
+++ Apps/Instructor/Instructor-REST-API/src/main/java/org/redquark/spring/instructor/course/CourseController.java
@@ -1,14 +1,20 @@
 package org.redquark.spring.instructor.course;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 /**
  * @author Anirudh Sharma
@@ -33,5 +39,24 @@ public class CourseController {
 			return ResponseEntity.noContent().build();
 		}
 		return ResponseEntity.notFound().build();
+	}
+
+	@GetMapping("/instructors/{name}/courses/{id}")
+	public Course getCourse(@PathVariable String name, @PathVariable long id) {
+		return service.findById(id);
+	}
+
+	@PutMapping("/instructors/{name}/courses/{id}")
+	public ResponseEntity<Course> updateCourse(@PathVariable String name, @PathVariable long id, @RequestBody Course course) {
+		Course courseUpdated = service.save(course);
+		return new ResponseEntity<>(courseUpdated, HttpStatus.OK);
+	}
+
+	@PostMapping("/instructors/{name}/courses")
+	public ResponseEntity<Void> createCourse(@PathVariable String name, @RequestBody Course course) {
+		Course createdCourse = service.save(course);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(createdCourse.getId())
+				.toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
