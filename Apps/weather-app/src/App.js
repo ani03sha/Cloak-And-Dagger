a1199f5d739fff6725c7a9ae62e59ecb34ec76c5
Anirudh Sharma <anirudh03sharma@gmail.com>
2020-03-10 19:40:44 +0530

    Added skeleton of components

--- Apps/weather-app/src/App.js
+++ Apps/weather-app/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
+import Title from './components/Title';
+import Form from './components/Form';
+import Weather from './components/Weather';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+  	return (
+    	<div className="App">
+      		<Title />
+      		<Form />
+      		<Weather />
+    		</div>
+  	);
 }
 
 export default App;
