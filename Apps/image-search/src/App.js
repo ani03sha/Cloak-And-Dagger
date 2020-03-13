596639917470898f3dad9e721d8d63e8107489dc
Anirudh Sharma <anirudh03sharma@gmail.com>
2020-03-13 23:16:33 +0530

    Added api call and styles

--- Apps/image-search/src/App.js
+++ Apps/image-search/src/App.js
@@ -1,9 +1,38 @@
-import React from "react"
+import React, {
+	Component
+} from "react"
+import ImageSearch from './ImageSearch/ImageSearch';
+import ImageList from './ImageList/ImageList';
 
-const App = () => (
-  <div>
-    <span role="img" aria-label="emoji">What up 😎</span>
-  </div>
-)
 
-export default App
+const API_KEY = "<API KEY>";
+
+class App extends Component {
+
+	state = {
+		images: []
+	};
+
+	handleGetRequest = async (e) => {
+		e.preventDefault();
+		const searchTerm = e.target.elements.searchValue.value;
+		const URL = `https://pixabay.com/api/?key=${API_KEY}&q=${searchTerm}&image_type=photo&pretty=true`
+		const request = await fetch(URL);
+		const response = await request.json();
+		this.setState({
+			images: response.hits
+		})
+		console.log(this.state.images);
+	};
+
+	render() {
+		return ( 
+			<div>
+				<ImageSearch handleGetRequest={this.handleGetRequest} />
+				<ImageList images={this.state.images} /> 
+			</div>
+		);
+	};
+}
+
+export default App;
