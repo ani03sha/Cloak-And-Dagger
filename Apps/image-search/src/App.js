1de921c14ed36576c7d4eb5f71093d9f831d6ef9
Anirudh Sharma <anirudh03sharma@gmail.com>
2020-03-13 23:58:29 +0530

    Added final code

--- Apps/image-search/src/App.js
+++ Apps/image-search/src/App.js
@@ -5,12 +5,13 @@ import ImageSearch from './ImageSearch/ImageSearch';
 import ImageList from './ImageList/ImageList';
 
 
-const API_KEY = "<API KEY>";
+const API_KEY = "YOUR API KEY";
 
 class App extends Component {
 
 	state = {
-		images: []
+		images: [],
+		error: null
 	};
 
 	handleGetRequest = async (e) => {
@@ -19,17 +20,29 @@ class App extends Component {
 		const URL = `https://pixabay.com/api/?key=${API_KEY}&q=${searchTerm}&image_type=photo&pretty=true`
 		const request = await fetch(URL);
 		const response = await request.json();
-		this.setState({
-			images: response.hits
-		})
-		console.log(this.state.images);
+		if(searchTerm) {
+			this.setState({
+				images: response.hits,
+				error: null
+			});
+		} else {
+			this.setState({
+				error: "Please enter a value"
+			});
+		}
 	};
 
 	render() {
 		return ( 
 			<div>
 				<ImageSearch handleGetRequest={this.handleGetRequest} />
-				<ImageList images={this.state.images} /> 
+				{
+					this.state.error !== null ? 
+					<div style={{ color: "#fff", textAlign:"center" }}>
+						{this.state.error}
+					</div> :
+					<ImageList images={this.state.images} /> 
+				}
 			</div>
 		);
 	};
