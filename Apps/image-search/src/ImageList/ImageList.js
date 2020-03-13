e3b8449d6d5d6be826684c497f95b1f4c6b2a57a
Anirudh Sharma <anirudh03sharma@gmail.com>
2020-03-13 23:17:17 +0530

    More changes

---
+++ Apps/image-search/src/ImageList/ImageList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './ImageList.css';
+
+const ImageList = (props) => {
+    return(
+        <div className="container">
+            <div className="row">
+                { props.images.map(({ id, largeImageURL, tags }) => {
+                    return (
+                        <div key={id} className="col-md-4" style={{marginBottom: "2rem"}}>
+                            < div className = "imageList__container" >
+                                <img className="imageList__image" src ={largeImageURL} alt={tags} />
+                            </div>
+                            <div className="image__details">
+                                <button>Search</button>
+                            </div>
+                        </div>
+                    );
+                }) 
+            }
+        </div>
+    </div>
+    );
+};
+
+export default ImageList;
---
+++ Apps/image-search/src/ImageSearch/ImageSearch.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ImageSearch = (props) => {
+    return(
+        <div>
+            <form onSubmit={props.handleGetRequest}>
+                <input type="text" autoComplete="off" name="searchValue" placeholder="Search for images" />
+                <button>Search</button>
+            </form>
+        </div>
+    );
+};
+
+export default ImageSearch;
