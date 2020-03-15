e62cb92255ebed170c0cdfcefdfa57e1261fdec4
Anirudh Sharma <anirudh03sharma@gmail.com>
2020-03-15 20:11:14 +0530

    Added code for API calls

--- Apps/express-weather/src/controllers/controller.js
+++ Apps/express-weather/src/controllers/controller.js
@@ -1,7 +1,22 @@
+const axios = require('axios');
+const API_KEY = 'YOUR API KEY';
+
 exports.renderHomePage = (request, response) => {
     response.render('index');
 }
 
 exports.renderAboutPage = (request, response) => {
     response.render('about');
+}
+
+exports.getWeather = (request, response) => {
+    const city = request.body.city;
+    const URL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`;
+    axios.get(URL).then((res) => {
+        response.render('index', {
+            weather: `It is currently ${res.data.main.temp} in ${res.data.name}`
+        })
+    }).catch((error) => {
+        console.log(error);
+    });
 }
--- Apps/express-weather/src/router.js
+++ Apps/express-weather/src/router.js
@@ -4,8 +4,7 @@ const controller = require('./controllers/controller');
 
 
 router.get('/', controller.renderHomePage);
-
 router.get('/about', controller.renderAboutPage);
-
+router.post('/', controller.getWeather);
 
 module.exports = router;
