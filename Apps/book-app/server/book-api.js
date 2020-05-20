d222025184aa697871aa1a1e725069ce2862c23a
Anirudh Sharma <anirudh03sharma@gmail.com>
2020-05-21 00:27:55 +0530

    Added logic to display list of books

--- Apps/book-app/server/book-api.js
+++ Apps/book-app/server/book-api.js
@@ -6,7 +6,30 @@ const app = express();
 const port = 3000;
 
 // Book array
-let books = [];
+let books = [{
+    "isbn": "9781593275846",
+    "title": "Eloquent JavaScript, Second Edition",
+    "author": "Marijn Haverbeke",
+    "publish_date": "2014-12-14",
+    "publisher": "No Starch Press",
+    "numOfPages": 472,
+},
+{
+    "isbn": "9781449331818",
+    "title": "Learning JavaScript Design Patterns",
+    "author": "Addy Osmani",
+    "publish_date": "2012-07-01",
+    "publisher": "O'Reilly Media",
+    "numOfPages": 254,
+},
+{
+    "isbn": "9781449365035",
+    "title": "Speaking JavaScript",
+    "author": "Axel Rauschmayer",
+    "publish_date": "2014-02-01",
+    "publisher": "O'Reilly Media",
+    "numOfPages": 460,
+}];
 
 // Enable CORS
 app.use(cors());
@@ -25,5 +48,9 @@ app.post('/book', (request, response) => {
 
     response.send('Book is added to the database');
 });
+
+app.get('/book', (request, response) => {
+    response.json(books);
+})
 
 app.listen(port, () => console.log(`Book app is listening on port ${port}!`));
